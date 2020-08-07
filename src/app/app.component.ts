781644705213c219e85e6840411c1d560c346268
Akshay9949 <akshay.murthy9949@gmail.com>
2020-08-07 09:09:12 +0530

    reactive

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
 import { Component } from '@angular/core';
-
+import {FormControl,FormGroup} from '@angular/forms';
+import { Validators } from '@angular/forms'
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'reactiveform';
+  
+  userprofileForm=new FormGroup({
+    firstname:new FormControl('',Validators.required),
+    lastname:new FormControl('',),
+    email_id:new FormControl('',Validators.required)
+  })
+  onSubmit(){
+    console.log(this.userprofileForm.value);
+    console.log(this.userprofileForm.controls['firstname'].value)
+
+  }
 }
